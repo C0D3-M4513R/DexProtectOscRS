1af1c2fa68082cd0e14ce76a7578f256e7b6ab2d
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2023-10-28 17:25:35 +0200

    flake test 1
    Signed-off-by: C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>

--- default.nix
+++ default.nix
@@ -1,49 +1,10 @@
-with import <nixpkgs>
-{
-  overlays = [
-    (import (fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
-  ];
-};
-
-let 
-  manifest = (pkgs.lib.importTOML ./app/Cargo.toml).package;
-  rustPlatform = makeRustPlatform {
-    cargo = pkgs.rust-bin.stable.latest.default;
-    rustc = pkgs.rust-bin.stable.latest.default;
-  };
-in
-rustPlatform.buildRustPackage rec {
-  pname = manifest.name;
-  version = manifest.version;
-  cargoLock.lockFile = ./Cargo.lock;
-  cargoLock.outputHashes = {
-    "egui_tracing-0.2.1" = "sha256-MWR5R5VwA7M+fkIU4vF01GsofMEYZ3IuQAbxVHSVWVY=";
-  };
-
-  src = pkgs.lib.cleanSource ./.;
-  nativeBuildInputs = [ pkgs.makeWrapper ];
-  buildFeatures = ["file_dialog"];
-  buildInputs = [ 
-    libGL
-    libxkbcommon
-    wayland
-    xorg.libX11
-    xorg.libXcursor
-    xorg.libXi
-    xorg.libXrandr
-  ];
-  libPath = with pkgs; lib.makeLibraryPath [
-    libGL
-    libxkbcommon
-    wayland
-    xorg.libX11
-    xorg.libXcursor
-    xorg.libXi
-    xorg.libXrandr
-  ];
-  LD_LIBRARY_PATH = libPath;
-  postInstall = ''
-    wrapProgram "$out/bin/$pname" --prefix LD_LIBRARY_PATH : "${libPath}"
-  '';
-}
-
+(import
+  (
+    let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  { src = ./.; }
+).defaultNix
---
+++ flake.nix
@@ -0,0 +1,73 @@
+{
+  inputs = {
+    naersk.url = "github:nmattia/naersk/master";
+    # This must be the stable nixpkgs if you're running the app on a
+    # stable NixOS install.  Mixing EGL library versions doesn't work.
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-compat = {
+      url = github:edolstra/flake-compat;
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, utils, naersk, rust-overlay, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {inherit system overlays;};
+        naersk-lib = pkgs.callPackage naersk {
+            cargo = pkgs.rust-bin.stable.latest.default;
+            rustc = pkgs.rust-bin.stable.latest.default;
+        };
+        manifest = (builtins.fromTOML (builtins.readFile ./app/Cargo.toml)).package;
+        libPath = with pkgs; lib.makeLibraryPath [
+          libGL
+          libxkbcommon
+          wayland
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+        ];
+      in
+      {
+        defaultPackage = naersk-lib.buildPackage {
+          src = pkgs.lib.cleanSource ./.;
+          doCheck = true;
+          pname = manifest.name;
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          buildInputs = with pkgs; [
+            pkgs.rust-bin.stable.latest.default
+            xorg.libxcb
+          ];
+          buildFeatures = ["file_dialog"];
+          postInstall = ''
+            wrapProgram "$out/bin/$pname" --prefix LD_LIBRARY_PATH : "${libPath}"
+          '';
+        };
+
+        defaultApp = utils.lib.mkApp {
+          drv = self.defaultPackage."${system}";
+        };
+
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            #cargo
+            cargo-insta
+            pre-commit
+            #rust-analyzer
+            #rustPackages.clippy
+            #rustc
+            #rustfmt
+            tokei
+
+            xorg.libxcb
+          ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          LD_LIBRARY_PATH = libPath;
+          GIT_EXTERNAL_DIFF = "${difftastic}/bin/difft";
+        };
+      });
+}
