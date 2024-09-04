4159a26424668355b2cfe7b32deddd5810942ba3
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-09-04 17:28:35 +0200

    update to nixos 24.05
    Signed-off-by: C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     naersk.url = "github:nmattia/naersk/master";
     # This must be the stable nixpkgs if you're running the app on a
     # stable NixOS install.  Mixing EGL library versions doesn't work.
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-compat = {
