497738d7bbe9ab24ab4113c308a28fb23294e2d2
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-09-05 17:25:13 +0200

    Update dependencies

--- app/src/main.rs
+++ app/src/main.rs
@@ -43,7 +43,7 @@ fn main() {
     if let Some(err) = eframe::run_native(
         "DexProtectOSC-RS",
         eframe::NativeOptions::default(),
-        Box::new(|cc| Box::new(app::App::new(collector, cc))),
+        Box::new(|cc| Ok(Box::new(app::App::new(collector, cc)))),
     )
         .err()
     {
