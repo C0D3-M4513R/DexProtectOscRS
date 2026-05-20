af769cc7277e88fd50b2be082cac03410a0c5975
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2026-05-20 17:29:19 +0200

    Fix byte parsing buffer not shrinking as expected

--- osc-handler/src/handlers/buffered_raw_packet_handler.rs
+++ osc-handler/src/handlers/buffered_raw_packet_handler.rs
@@ -4,6 +4,7 @@ use crate::ArbitraryHandler;
 ///Wraps a [crate::RawPacketHandler] into a [crate::BufferedRawPacketHandler]
 #[derive(Debug)]
 pub struct BufferedRawPacketHandler<H> {
+    prev_buffer: crate::Vec<u8>, //Temp buffer to store the leftover of the last parse
     buffer: crate::Vec<u8>,
     max_buffer_size: Option<NonZeroUsize>,
     pub handler: H
@@ -12,7 +13,8 @@ pub struct BufferedRawPacketHandler<H> {
 impl<H> BufferedRawPacketHandler<H> {
     /// Create a new instance of a [BufferedRawPacketHandler]
     pub const fn new(handler: H, max_buffer_size: Option<NonZeroUsize>,) -> Self {
-        Self { 
+        Self {
+            prev_buffer: crate::Vec::new(),
             buffer: crate::Vec::new(),
             max_buffer_size,
             handler 
@@ -30,15 +32,16 @@ impl<H: crate::RawPacketHandler> ArbitraryHandler<&'_ [u8]> for BufferedRawPacke
     type Output = crate::Vec<H::Output>;
 
     fn handle(&mut self, message: &'_ [u8]) -> Self::Output {
+        self.prev_buffer.clear();
         self.buffer.extend_from_slice(message);
         let mut buf = self.buffer.as_slice();
         let mut res = crate::Vec::new();
         loop {
-            let (r, fut) = self.handler.handle(self.buffer.as_slice());
+            let (r, fut) = self.handler.handle(buf);
             res.push(fut);
-            
+
             //If the last call did not process any bytes, assume we are done for now.
-            if r.len() == buf.len() {
+            if r.len() == buf.len() || r.len() == 0 {
                 let mut buf = r;
                 if let Some(max_buffer_size) = self.max_buffer_size {
                     if r.len() >= max_buffer_size.get() {
@@ -46,7 +49,11 @@ impl<H: crate::RawPacketHandler> ArbitraryHandler<&'_ [u8]> for BufferedRawPacke
                         buf = &[];
                     }
                 }
-                self.buffer = crate::Vec::from(buf);
+                if buf.len() > self.buffer.len() {
+                    log::error!("Buffer grew whilst parsing?");
+                }
+                self.prev_buffer.extend_from_slice(buf);
+                std::mem::swap(&mut self.buffer, &mut self.prev_buffer);
                 return res;
             }
 
